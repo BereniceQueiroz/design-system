fc221e3d36cd8ed00926513d145bc270c18c94e2
BereniceQueiroz <baqueiroz14@gmail.com>
2023-06-26 18:58:59 -0300

    feat: component textarea

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+  display: 'flex',
+  alignItems: 'baseline',
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$white',
+  fontWeight: '$regular',
+  resize: 'vertical',
+  minHeight: 80,
+
+  '&:has(input:focus)': {
+    outline: 0,
+    borderColor: '$green300',
+  },
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
